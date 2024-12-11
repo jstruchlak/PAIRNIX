752efa7aaa897436f1282025e7d63fa201bf7038
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-12-11 16:13:53 +1030

    Added Base API controler, Password hashing and salt algorithm and DTO

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6524bbdc10129d2bac31460f25f8abb669e5bc02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3df7da1d846d0389f158bf949a6adae97e03a595")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
