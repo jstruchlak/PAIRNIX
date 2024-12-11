36d2fd8b364c6087bdf6cb99f765e2a8d8814dbf
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-12-11 11:14:44 +1030

    new

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ab406fb87fe7136df1463786d3f0d9a5c5f488f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+554b39671e58fb85703dba805a2f69756afbc7d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
