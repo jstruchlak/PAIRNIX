554b39671e58fb85703dba805a2f69756afbc7d3
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-12-11 10:38:18 +1030

    Added Angular config and Cors

--- API/Program.cs
+++ API/Program.cs
@@ -5,13 +5,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => {
+builder.Services.AddDbContext<DataContext>(opt =>
+{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigins", policy =>
+    {
+        policy.AllowAnyHeader()
+              .AllowAnyMethod()
+              .WithOrigins("http://localhost:4200", "https://localhost:4200"); // Corrected origins syntax
+    });
 });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline - Middleware
+app.UseCors("AllowSpecificOrigins");
 app.MapControllers();
 
 app.Run();
---
+++ README.learn.md
@@ -0,0 +1,10 @@
+# LEARNING GOALS (.NET)
+1.Using dotnet CLI
+2.API Controllers and Endpoints
+3.Entity Framework
+4.Config Envrionment Varibales
+
+
+
+
+# LEARNING GOALS (Angular)
