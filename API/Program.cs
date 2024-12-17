09b2d317d02119aa95dbfc018e042cee711c5fce
Struchlak, Joey (ECSA) <Joey.Struchlak@sa.gov.au>
2024-12-17 12:56:26 +1030

    Error Handling for the application

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline - Middleware
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors("AllowSpecificOrigins");
 
 app.UseAuthentication();
